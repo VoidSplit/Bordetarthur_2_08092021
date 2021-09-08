0e5a3b261c055cf5aa3a65e125a24dc072769636
VoidSplit <magiksguys@gmail.com>
2021-09-08 17:01:12 +0200

    First Commit
    This first commit contains part of the desktop version

---
+++ ressources/js/app.js
@@ -0,0 +1,10 @@
+/*document.querySelectorAll(".stars-count").forEach(counters => {
+    let note = counters.getAttribute("data-stars");
+    console.log(counters);
+    console.log(note);
+    for(let i=0; i<note; i++) {
+        //let star = document.querySelectorAll(`${counters} i`)[i];
+        console.log(counters.childNodes);
+       // star.style.color = "red";
+    }
+});*/
